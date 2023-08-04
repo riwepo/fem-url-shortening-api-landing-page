3b1d865b812f4f1dd3ddd40332acf4fd7a2596ab
Richard Potter <richardpotter.au@gmail.com>
2023-08-04 15:29:49 +1000

    basic structure of hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+import illustrationWorking from "../images/illustration-working.svg";
+
 import classes from "./Hero.module.css";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className={classes.container}>
+      <img
+        className={classes.illustrationWorking}
+        src={illustrationWorking}
+        alt="woman working at desk"
+      />
+      <h1 className={classes.heading}>More than just shorter links</h1>
+      <p className={classes.text}>
+        Build your brand's recognition and get detailed insights on how your
+        links are performing.
+      </p>
+      <button className={classes.button}>Get Started</button>
+    </div>
+  );
 }
 
 export default Hero;
