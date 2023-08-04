8ceaf47a20db972119b797cc4618b3992827964b
Richard Potter <richardpotter.au@gmail.com>
2023-08-05 09:07:58 +1000

    styling stats

--- src/components/statistics/StatsItem.js
+++ src/components/statistics/StatsItem.js
@@ -5,16 +5,17 @@ import classes from "./StatsItem.module.css";
 function StatsItem({ itemInfo }) {
   const createImgSource = (iconFileName) => {
     const source = `${process.env.PUBLIC_URL}/images/${iconFileName}`;
-    console.log(source);
     return source;
   };
   return (
     <div className={classes.container}>
-      <img
-        className={classes.img}
-        src={createImgSource(itemInfo.iconFileName)}
-        alt={itemInfo.iconAlt}
-      />
+      <div className={classes.imageContainer}>
+        <img
+          className={classes.img}
+          src={createImgSource(itemInfo.iconFileName)}
+          alt={itemInfo.iconAlt}
+        />
+      </div>
       <h2 className={classes.heading}>{itemInfo.heading}</h2>
       <p className={classes.text}>{itemInfo.text}</p>
     </div>
--- src/components/statistics/StatsItemList.js
+++ src/components/statistics/StatsItemList.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import StatsItem from "./StatsItem";
 
-import classes from "./StatsItem.module.css";
+import classes from "./StatsItemList.module.css";
 
 function StatsItemList({ items }) {
   return (
-    <ul>
+    <ul className={classes.container}>
       {items.map((item) => (
         <StatsItem key={item.id} itemInfo={item} />
       ))}
