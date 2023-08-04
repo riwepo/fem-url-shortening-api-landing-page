7423a47f4f23d84a2874e0feabac504b357d1d55
Richard Potter <richardpotter.au@gmail.com>
2023-08-04 15:11:16 +1000

    basic mobile navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+import logo from "../images/logo.svg";
+import openMenu from "../images/open-menu-6208.svg";
+
 import classes from "./NavBar.module.css";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className={classes.container}>
+      <img className={classes.logo} src={logo} alt="company logo" />
+      <img
+        className={classes.openMenu}
+        src={openMenu}
+        alt="hamburger menu icon"
+      />
+    </div>
+  );
 }
 
 export default NavBar;
