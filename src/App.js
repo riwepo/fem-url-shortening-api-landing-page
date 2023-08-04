1c8e746f59051ce3765bbbcf9e685be171d25fbb
Richard Potter <richardpotter.au@gmail.com>
2023-08-04 14:54:49 +1000

    lay out components in flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Shorten from "./components/Shorten";
+import Stats from "./components/Stats";
+import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer";
+
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={classes.container}>
+      <NavBar />
+      <Hero />
+      <Shorten />
+      <Stats />
+      <CallToAction />
+      <Footer />
+    </main>
   );
 }
 
