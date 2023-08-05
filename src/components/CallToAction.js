2f13642d06491d7c029d6d304762756ed169057b
Richard Potter <richardpotter.au@gmail.com>
2023-08-05 11:03:40 +1000

    styled call to action

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+import GetStartedButton from "./GetStartedButton";
+
 import classes from "./CallToAction.module.css";
 
 function CallToAction() {
-  return <div>Call to action</div>;
+  return (
+    <div className={classes.container}>
+      <h2 className={classes.heading}>Boost your links today</h2>
+      <GetStartedButton className={classes.button} />
+    </div>
+  );
 }
 
 export default CallToAction;
