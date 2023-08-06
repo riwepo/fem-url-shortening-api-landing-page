ec5b7bcdc349ac70f61752db6de0fd15548b5d79
Richard Potter <richardpotter.au@gmail.com>
2023-08-06 11:46:04 +1000

    put stuff into main

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Hero from "./Hero";
+import ShortenForm from "./ShortenForm";
+import Stats from "./statistics/Stats";
+import CallToAction from "./CallToAction";
+
+import classes from "./Main.module.css";
+
+function Main() {
+  return (
+    <main className={classes.main}>
+      <Hero />
+      <ShortenForm />
+      <Stats />
+      <CallToAction />
+    </main>
+  );
+}
+
+export default Main;
