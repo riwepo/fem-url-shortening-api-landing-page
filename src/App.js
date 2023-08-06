6fb041c5f30923fd37f327adb9175472799f4cde
Richard Potter <richardpotter.au@gmail.com>
2023-08-06 11:46:24 +1000

    refactor into header, main and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from "react";
 
 import Header from "./components/Header";
-import Hero from "./components/Hero";
-import ShortenForm from "./components/ShortenForm";
-import Stats from "./components/statistics/Stats";
-import CallToAction from "./components/CallToAction";
+import Main from "./components/Main";
 import Footer from "./components/footer/Footer";
 
 import classes from "./App.module.css";
 
 function App() {
   return (
-    <main className={classes.container}>
+    <div className={classes.container}>
       <Header />
-      <Hero />
-      <ShortenForm />
-      <Stats />
-      <CallToAction />
+      <Main />
       <Footer />
-    </main>
+    </div>
   );
 }
 
