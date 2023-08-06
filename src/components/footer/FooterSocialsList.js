a8fde59148a8f659d251bf295dc5eb5476480d09
Richard Potter <richardpotter.au@gmail.com>
2023-08-06 11:27:18 +1000

    style active states social icons

--- src/components/footer/FooterSocialsList.js
+++ src/components/footer/FooterSocialsList.js
@@ -1,42 +1,34 @@
 import React from "react";
 
-import iconFacebook from "../../images/icon-facebook.svg";
-import iconTwitter from "../../images/icon-twitter.svg";
-import iconPinterest from "../../images/icon-pinterest.svg";
-import iconInstagram from "../../images/icon-instagram.svg";
+import { ReactComponent as IconFacebook } from "../../images/icon-facebook.svg";
+import { ReactComponent as IconTwitter } from "../../images/icon-twitter.svg";
+import { ReactComponent as IconPinterest } from "../../images/icon-pinterest.svg";
+import { ReactComponent as IconInstagram } from "../../images/icon-instagram.svg";
 
 import classes from "./FooterSocialsList.module.css";
 
 function FooterSocialsList() {
   return (
     <ul className={classes.list}>
-      <li>
-        <img
-          className={classes.img}
-          src={iconFacebook}
-          alt="social media icon"
-        />
+      <li key={1}>
+        <a className={classes.a} href="#facebook">
+          <IconFacebook className={classes.icon} />
+        </a>
       </li>
-      <li>
-        <img
-          className={classes.img}
-          src={iconTwitter}
-          alt="social media icon"
-        />
+      <li key={2}>
+        <a className={classes.a} href="#twitter">
+          <IconTwitter className={classes.icon} />
+        </a>
       </li>
-      <li>
-        <img
-          className={classes.img}
-          src={iconPinterest}
-          alt="social media icon"
-        />
+      <li key={3}>
+        <a className={classes.a} href="#pinterest">
+          <IconPinterest className={classes.icon} />
+        </a>
       </li>
-      <li>
-        <img
-          className={classes.img}
-          src={iconInstagram}
-          alt="social media icon"
-        />
+      <li key={4}>
+        <a className={classes.a} href="#instagram">
+          <IconInstagram className={classes.icon} />
+        </a>
       </li>
     </ul>
   );
