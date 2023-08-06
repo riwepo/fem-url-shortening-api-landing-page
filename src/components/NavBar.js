25beb4531d4b89cd227e7a50231db47566d36ed7
Richard Potter <richardpotter.au@gmail.com>
2023-08-07 09:52:41 +1000

    add an overlay to close on click away

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,22 +4,25 @@ import Button from "./Button";
 
 import classes from "./NavBar.module.css";
 
-function NavBar() {
+function NavBar({ onOverlayClicked }) {
   return (
-    <nav className={classes.container}>
-      <a className={classes.a} href="#features">
-        Features
-      </a>
-      <a className={classes.a} href="#pricing">
-        Pricing
-      </a>
-      <a className={classes.a} href="#resources">
-        Resources
-      </a>
-      <div className={classes.separator}></div>
-      <button className={classes.loginButton}>Login</button>
-      <Button className={classes.signupButton} label="Sign Up" />
-    </nav>
+    <>
+      <nav className={classes.container}>
+        <a className={classes.a} href="#features">
+          Features
+        </a>
+        <a className={classes.a} href="#pricing">
+          Pricing
+        </a>
+        <a className={classes.a} href="#resources">
+          Resources
+        </a>
+        <div className={classes.separator}></div>
+        <button className={classes.loginButton}>Login</button>
+        <Button className={classes.signupButton} label="Sign Up" />
+      </nav>
+      <div className={classes.overlay} onClick={onOverlayClicked}></div>
+    </>
   );
 }
 
