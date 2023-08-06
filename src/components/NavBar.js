4284d989491300e7dc207cec05110802ec000201
Richard Potter <richardpotter.au@gmail.com>
2023-08-07 08:37:13 +1000

    initial implementation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,25 @@
 import React from "react";
 
+import Button from "./Button";
+
 import classes from "./NavBar.module.css";
 
 function NavBar() {
   return (
-    <div className={classes.container}>
-      <div>Navbar</div>
-    </div>
+    <nav className={classes.container}>
+      <a className={classes.a} href="#features">
+        Features
+      </a>
+      <a className={classes.a} href="#pricing">
+        Pricing
+      </a>
+      <a className={classes.a} href="#resources">
+        Resources
+      </a>
+      <div className={classes.separator}></div>
+      <button className={classes.loginButton}>Login</button>
+      <Button className={classes.signupButton} label="Sign Up" />
+    </nav>
   );
 }
 
