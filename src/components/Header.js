085352d6b03b31a4df0f3d103fec5daf189d34cc
Richard Potter <richardpotter.au@gmail.com>
2023-08-07 09:08:38 +1000

    open / close menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NavBar from "./NavBar";
 
-import logo from "../images/logo.svg";
-import openMenu from "../images/open-menu-6208.svg";
+import { ReactComponent as Logo } from "../images/logo.svg";
+import { ReactComponent as OpenMenu } from "../images/open-menu-6208.svg";
 
 import classes from "./Header.module.css";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const openMenuClickHandler = () => {
+    setIsMenuOpen((current) => !current);
+  };
   return (
     <header className={classes.container}>
-      <img className={classes.logo} src={logo} alt="company logo" />
-      <NavBar />
-      <img
-        className={classes.openMenu}
-        src={openMenu}
-        alt="hamburger menu icon"
+      <Logo className={classes.logo} />
+      {isMenuOpen && <NavBar />}
+      <OpenMenu
+        className={`${classes.openMenu} ${
+          isMenuOpen ? classes.openMenuIsOpen : null
+        }`}
+        onClick={openMenuClickHandler}
       />
     </header>
   );
