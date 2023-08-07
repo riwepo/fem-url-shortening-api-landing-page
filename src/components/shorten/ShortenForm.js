28a19b31c9dca9ed82aae1819501e7ca608e629f
Richard Potter <richardpotter.au@gmail.com>
2023-08-07 11:38:39 +1000

    validate link submitted pass back on submit

--- src/components/shorten/ShortenForm.js
+++ src/components/shorten/ShortenForm.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./ShortenForm.module.css";
 
-function ShortenForm() {
+function ShortenForm({ onLinkSubmitted }) {
+  const [enteredUrl, setEnteredUrl] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (!isValid) return;
+    onLinkSubmitted(enteredUrl);
+    setEnteredUrl("");
+  };
+
+  const inputChangeHandler = (event) => {
+    const localValue = event.target.value;
+    setEnteredUrl(localValue);
+    // add more validation??
+    if (localValue !== "") {
+      setIsValid(true);
+      setIsError(false);
+    } else {
+      setIsError(true);
+      setIsValid(false);
+    }
+  };
   return (
     <div className={classes.container}>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <input
-          className={classes.input}
+          className={`${classes.input} ${isError ? classes.inputError : null}`}
           type="text"
           placeholder="Shorten a link here..."
+          onChange={inputChangeHandler}
         />
-        <button className={classes.button}>Shorten It!</button>
+        {isError && <p className={classes.errorMessage}>Please add a link</p>}
+        <button className={classes.button} disabled={!isValid}>
+          Shorten It!
+        </button>
       </form>
     </div>
   );
