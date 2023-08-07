8e39fa0c6df6b3b2bba6fe6fc7c102ff1b7a7211
Richard Potter <richardpotter.au@gmail.com>
2023-08-07 11:06:07 +1000

    handle is copied

--- src/components/shorten/ShortenedUrl.js
+++ src/components/shorten/ShortenedUrl.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./ShortenedUrl.module.css";
 
 function ShortenedUrl({ shortenedUrl }) {
+  const [isCopied, setIsCopied] = useState(false);
+  const copyClickHandler = (event) => {
+    setIsCopied(true);
+  };
   return (
     <div className={classes.container}>
       <div className={classes.original}>{shortenedUrl.original}</div>
       <div className={classes.shortened}>{shortenedUrl.shortened}</div>
-      <button className={classes.button}>Copy</button>
+      <button
+        className={`${classes.button} ${
+          isCopied ? classes.buttonCopied : null
+        }`}
+        onClick={copyClickHandler}
+        disabled={isCopied}
+      >
+        {!isCopied ? "Copy" : "Copied!"}
+      </button>
     </div>
   );
 }
